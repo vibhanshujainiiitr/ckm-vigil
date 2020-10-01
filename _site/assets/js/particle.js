ca34c67a276a6d1cd02c8b4d2cef0d2e01c9d6ca
Vibhanshu Jain <cs19b1027@iiitr.ac.in>
2020-10-01 14:30:17 +0530

    New logo added | Header updated

--- _site/assets/js/particle.js
+++ _site/assets/js/particle.js
@@ -1,95 +1,102 @@
-const particles = {
+particlesJS("particles-js", {
 	"particles": {
-			"number": {
-				"value": 50,
-				"density": {
-					"enable": true,
-					"value_area": 900 
-				}
-			},
-			"color": {
-				"value": "#000000"
-			},
-			"shape": {
-				"type": "circle", 
-				"stroke": {
-					"width": 0.5,
-					"color": "#000000"
-				},
-				"polygon": { 
-					"nb_sides": 5
-				}
-			},
-			"opacity": {
-				"value": 0.5,
-				"random": true
-			},
-			"size": {
-				"value": 9,
-				"random": true
-			},
-			"line_linked": {
-				"enable": true,
-				"distance": 200, 
-				"color": "#000000",
-				"opacity": 0.5,
-				"width": 2
-			},
-			"move": {
-				"enable": true,
-				"speed": 2,
-				"direction": "top",
-				"random": true,
-				"straight": false,
-				"out_mode": "out", 
-				"bounce": false, 
-				"attract": {
-					"enable": true,
-					"rotateX": 800,
-					"rotateY": 1400
-				}
-			}
+	  "number": {
+		"value": 120,
+		"density": {
+		  "enable": true,
+		  "value_area": 800
+		}
+	  },
+	  "color": {
+		"value": "#ffffff"
+	  },
+	  "shape": {
+		"type": "circle",
+		"stroke": {
+		  "width": 1,
+		  "color": "#33a0ff"
 		},
-	  "interactivity": {
-			"detect_on": "canvas",
-			"events": {
-				"onhover": {
-					"enable": false,
-					"mode": "repulse"
-				},
-				"onclick": {
-					"enable": false,
-					"mode": "push"
-				},
-				"resize": true
-			},
-			"modes": {
-				"grab": {
-					"distance": 800,
-					"line_linked": {
-						"opacity": 1
-					}
-				},
-				"bubble": {
-					"distance": 800,
-					"size": 80,
-					"duration": 2,
-					"opacity": 0.8,
-					"speed": 3
-				},
-				"repulse": {
-					"distance": 400,
-					"duration": 0.4
-				},
-				"push": {
-					"particles_nb": 4
-				},
-				"remove": {
-					"particles_nb": 2
-				}
-			}
+	  },
+	  "opacity": {
+		"value": 0.5,
+		"random": false,
+		"anim": {
+		  "enable": false,
+		  "speed": 1,
+		  "opacity_min": 0.1,
+		  "sync": false
+		}
+	  },
+	  "size": {
+		"value": 5,
+		"random": true,
+		"anim": {
+		  "enable": false,
+		  "speed": 40,
+		  "size_min": 0.1,
+		  "sync": false
+		}
+	  },
+	  "line_linked": {
+		"enable": true,
+		"distance": 150,
+		"color": "#4dacff",
+		"opacity": 0.4,
+		"width": 1
+	  },
+	  "move": {
+		"enable": true,
+		"speed": 6,
+		"direction": "none",
+		"random": false,
+		"straight": false,
+		"out_mode": "out",
+		"bounce": false,
+		"attract": {
+		  "enable": false,
+		  "rotateX": 600,
+		  "rotateY": 1200
+		}
+	  }
+	},
+	"interactivity": {
+	  "detect_on": "canvas",
+	  "events": {
+		"onhover": {
+		  "enable": true,
+		  "mode": "grab"
 		},
-		"retina_detect": true
-	}
-	
-	particlesJS("particles", particles)
+		"onclick": {
+		  "enable": true,
+		  "mode": "push"
+		},
+		"resize": true
+	  },
+	  "modes": {
+		"grab": {
+		  "distance": 140,
+		  "line_linked": {
+			"opacity": 1
+		  }
+		},
+		"bubble": {
+		  "distance": 400,
+		  "size": 40,
+		  "duration": 2,
+		  "opacity": 8,
+		  "speed": 3
+		},
+		"repulse": {
+		  "distance": 200,
+		  "duration": 0.4
+		},
+		"push": {
+		  "particles_nb": 4
+		},
+		"remove": {
+		  "particles_nb": 2
+		}
+	  }
+	},
+	"retina_detect": true
+  });
